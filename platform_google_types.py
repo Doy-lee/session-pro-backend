d03b9673a7c0143262584a9908bf463b4b2e4834
Ryan Miller <aerilym@pm.me>
2025-10-09 15:55:54 +1100

    chore: add __repr__ methods to google time and duration classes

--- platform_google_types.py
+++ platform_google_types.py
@@ -2,7 +2,7 @@ import dataclasses
 import traceback
 import typing
 from enum import IntEnum, StrEnum
-from typing import Optional
+from typing import Optional, override
 
 from google.protobuf.internal.well_known_types import Timestamp
 
@@ -23,6 +23,9 @@ class GoogleTimestamp(Timestamp):
         except Exception as e:
             err.msg_list.append(f'Failed to parse timestamp "{rfc3339_timestamp}": {traceback.format_exc()}')
 
+    @override
+    def __repr__(self):
+        return f"GoogleTimestamp('{self.rfc3339}', unix={self.unix_seconds})"
 
 # Copied from google.protobuf.internal.well_known_types
 _DURATION_SECONDS_MAX = 315576000000
@@ -41,7 +44,11 @@ class GoogleDuration():
             self.milliseconds = self.seconds * 1000
         except Exception as e:
             err.msg_list.append(f'Failed to parse duration "{iso8601_duration}: {traceback.format_exc()}"')
-    
+
+    @override
+    def __repr__(self):
+        return f"GoogleDuration('{self.iso8601}', ms={self.milliseconds})"
+
     def FromJsonString(self, value: str) -> int:
         """Parse a ISO 8601 Duration string format to a Duration.
         NOTE: this parser is not fully compliant with ISO 8601, it is intended for use
